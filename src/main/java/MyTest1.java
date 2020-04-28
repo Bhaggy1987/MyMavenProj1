85a29d84864c45fdde153a245561f966fabc0450
Bhaggy1987 <Bhagavatheeswaran.m@gmail.com>
2020-04-28 18:14:25 -0400

    new file added

---
+++ src/main/java/MyTest1.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class MyTest1 {
+
+    @Test
+    public void myTest1(){
+        System.out.println("Test Case1 MODIFIEDnew");
+        System.out.println("new line");
+    }
+
+    @Test
+    public void myTest2(){
+        System.out.println("Test Case1 MODIFIEDnew");
+        System.out.println("new line");
+    }
+
+}
---
+++ src/main/java/MyTest2.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class MyTest2 {
+
+
+    @Test
+    public void myTest1(){
+        System.out.println("Test Case1 MODIFIEDnew");
+        System.out.println("new line");
+    }
+
+    @Test
+    public void myTest2(){
+        System.out.println("Test Case1 MODIFIEDnew");
+        System.out.println("new line");
+    }
+    
+}
