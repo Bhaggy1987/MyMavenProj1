3d9466c44df4219cc924aaddee5d32f84d993578
Bhaggy1987 <Bhagavatheeswaran.m@gmail.com>
2020-04-28 17:40:15 -0400

    Feature1_Bhaggy_04282020

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -6,6 +6,12 @@ public class MyTest {
     @Test
     public void myTest1(){
         System.out.println("Test Case1 MODIFIEDnew");
+        System.out.println("new line");
     }
 
+    @Test
+    public void myTest2(){
+        System.out.println("Test Case1 MODIFIEDnew");
+        System.out.println("new line");
+    }
 }
